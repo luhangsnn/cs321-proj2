a59be840faf05e628647b5f487e857fd50baf7f5
RPSeaman <rpseam22@colby.edu>
2021-10-20 17:38:02 -0400

    start of postfix.java and adjustment to LLstack.java

--- LLstack.java
+++ LLstack.java
@@ -1,4 +1,4 @@
-import java.util.EmptyStackException;
+
 
 public class LLstack <T> {
 
---
+++ Postfix.java
@@ -0,0 +1,23 @@
+// CS321 Project 2
+// Postfix.java
+
+import LLstack;
+
+
+public class Postfix < T > {
+
+    public S LLstack;
+
+    public  File();
+
+
+    // constructor
+
+
+
+
+
+
+
+
+}
