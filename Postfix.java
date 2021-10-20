93ff19163911f0d800a84483f568d4a7d75b410d
Luhang Sun <luhangsun@outlook.com>
2021-10-20 17:44:23 -0400

    outlining the structure of the class

--- Postfix.java
+++ Postfix.java
@@ -2,21 +2,29 @@
 // Postfix.java
 
 import LLstack;
+import java.io.File;
 
 
-public class Postfix < T > {
+public class Postfix <T> {
 
     public S LLstack;
-
-    public  File();
-
+    public File input;
+    public File output;
 
     // constructor
+    public Postfix(){
 
+    }
 
+    // output needs to be specified as file 
+    public voids readFile(){
 
+    }
 
+    // also needs to change to specified object name of returns 
+    public void infixToPostfix(){
 
+    }
 
 
 
