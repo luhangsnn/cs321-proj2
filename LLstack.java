46b78f24ab8518c9f4a5037de8b57c9fe57fa0f8
RPSeaman <rpseam22@colby.edu>
2021-10-20 20:35:41 -0400

    additions to infixToPostfix and adjustment to LLstack (comments and tests)

--- LLstack.java
+++ LLstack.java
@@ -1,4 +1,7 @@
-
+// CS321 Project 2
+// LLstack.java
+// Ryan Seaman and Luhang Sun
+// October 20, 2021
 
 public class LLstack <T> {
 
@@ -42,17 +45,17 @@ private class Node {
         size = 0;
     }
     
-// check if the stack is empty
+// This checks if the stack is empty.
 public boolean isEmpty () {
  return top == null;
  }
 
-// get the number of elements in the stack
+// This returns the size of the stack.
 public int size () {
  return size;
  }
 
-    // push a node to the stack
+    // This pushes (adds) a new node to the stack.
     public void push (T val) {
         Node node = new Node(val, top);
         top = node;
@@ -60,19 +63,19 @@ public int size () {
     }
 
 
-// return the top element and remove it from the stack
+// This returns and removes the top node in the stack
 public T pop () {
  T data = null;
  if (isEmpty()) 
  {
-    System.out.println("Stack is empty!");
+    System.out.println("Stack is empty!"); // Print statement if the stack is empty.
  }
 
     else {
-        data = top.getData(); // get the data stored in the top element
-        Node tmp = top; // set a tmp pointer points to the top element
-        top = top.getNext(); // update the top pointer
-        size--;
+        data = top.getData(); // Sets the data from the top node to data variable.
+        Node tmp = top; // This sets a temporary pointer to the top node. 
+        top = top.getNext(); // This updates the top pointer.
+        size--; // this decrements the size by one.
     }
 
     return data;
@@ -88,25 +91,28 @@ public T pop () {
 
 public static void main (String[] args) {
 
- LLstack <String> string_stack = new LLstack<String>();
+ LLstack <String> string_stack = new LLstack<String>(); // creates a new LL. 
 
- System.out.println("If the stack is empty: " + string_stack.isEmpty());
+ System.out.println("If the stack is empty: " + string_stack.isEmpty()); // Tests to see if the linked list is empty. 
 
- string_stack.push("a");
- string_stack.push("b");
- string_stack.push("ab123");
+ string_stack.push("a"); // pushes 3 new nodes (strings)
+ string_stack.push("3");
+ string_stack.push(")");
+ string_stack.push("2 + 3 + 5");
+ string_stack.push("(23 + (5 * 10)");
 
- System.out.println("Size of the stack: " + string_stack.size());
+
+ System.out.println("Size of the stack: " + string_stack.size()); // Checks the size of the stack
 //  System.out.println("Top element: " + string_stack.peek());
 
 //  System.out.println("*** Testing pop ***");
  while (!string_stack.isEmpty()){
- System.out.println("Now popping: "+ string_stack.pop());
+ System.out.println("Now popping: "+ string_stack.pop()); // pops everything in the stack
  }
 
- System.out.println("After popping, size of the stack: " + string_stack.size());
+ System.out.println("After popping, size of the stack: " + string_stack.size()); // checks to see that everything was removed. 
 
 //  keep popping after the stack is empty
-string_stack.pop();
+string_stack.pop(); // tries to pop after stack is already empty. Should have a print statement saying stack is empty. 
  }
 }
--- Postfix.java
+++ Postfix.java
@@ -1,5 +1,7 @@
 // CS321 Project 2
 // Postfix.java
+// Ryan Seaman and Luhang Sun
+// October 20, 2021
 
 import LLstack;
 import java.io.File;
@@ -10,6 +12,7 @@ public class Postfix <T> {
     public S LLstack;
     public File input;
     public File output;
+    public Tok token;
 
     // constructor
     public Postfix(){
@@ -22,8 +25,27 @@ public class Postfix <T> {
     }
 
     // also needs to change to specified object name of returns 
-    public void infixToPostfix(){
+    public void infixToPostfix(Tok token){
+        // read token from readFile line of code.
 
+        // new empty stack
+        LLstack <String> transferStack = new LLstack<String>();
+
+        while (token! = ";") {
+            if (token == ")") {
+                right = transferStack.pop();
+                oper = transferStack.pop();
+                left = transferStack.pop();
+                push(left + right + oper);
+            }
+            else {
+                if (token != "("){
+                    transferStack.push(token);
+                }
+            // read next token
+            }
+        }
+        // top of stack is a postfix expression
     }
 
 
